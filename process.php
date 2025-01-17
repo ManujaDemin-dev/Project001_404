b2bee9a8e5ba8f249f40131d1c236c2ae8e15e83
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-01-17 16:05:21 +0530

    Login page

---
+++ process.php
@@ -0,0 +1,22 @@
+<?php
+include("server.php");
+session_start();
+
+$e = $_POST['email'];
+$pw = $_POST['pw'];
+
+$sql = "SELECT * FROM projectdb WHERE Email = '$e' AND PWD = '$pw' ";
+
+$rn = mysqli_query($conn , $sql);
+
+if(mysqli_num_rows($rn)> 0){
+    $_SESSION['username'] = [$e];
+    header("Location: welcome.php");
+
+}else{
+    header("Location: login.html");
+
+}
+
+
+?>
