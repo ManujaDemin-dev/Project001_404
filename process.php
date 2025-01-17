d3382e13fba2865a7178ab8207f84bf63432aed8
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-01-17 16:22:02 +0530

    const user dashboard

--- process.php
+++ process.php
@@ -10,7 +10,7 @@ $sql = "SELECT * FROM projectdb WHERE Email = '$e' AND PWD = '$pw' ";
 $rn = mysqli_query($conn , $sql);
 
 if(mysqli_num_rows($rn)> 0){
-    $_SESSION['username'] = [$e];
+    $_SESSION['user'] = "user";
     header("Location: welcome.php");
 
 }else{
--- welcome.php
+++ welcome.php
@@ -1,5 +1,28 @@
 <?php
-echo "welcomeee";
+session_start();
 
+if (!isset($_SESSION['username'])) {
+    header('Location: login.html');
+    exit();
+}
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Dashboard</title>
+</head>
+<body>
+    <div class="dashboard">
+        <h1>Welcome, <?php echo "yow" . $_SESSION["user"]; ?>!</h1>
+        <p>You have successfully logged in.</p>
+        <form action="logout.php" method="POST">
+            <button type="submit" name="logout">Log Out</button>
+        </form>
+    </div>
+</body>
+</html>
