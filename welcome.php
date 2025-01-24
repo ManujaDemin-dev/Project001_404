62ac976356f409052f648dc21758b4e16d741b07
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-01-24 13:03:08 +0530

    db config online

--- welcome.php
+++ welcome.php
@@ -14,12 +14,13 @@ if (!isset($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Dashboard</title>
+    <title><?php echo $_SESSION['username']?> Dashboard</title>
 </head>
 <body>
     <div class="dashboard">
         <h1>Welcome, <?php echo $_SESSION["username"]; ?>!</h1>
         <p>You have successfully logged in.</p>
+        <p>Glad to have you on board</p>
         <form action="logout.php" method="POST">
             <button type="submit" name="logout">Log Out</button>
         </form>
